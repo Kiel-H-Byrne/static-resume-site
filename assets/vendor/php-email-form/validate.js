ded06e63d4c23e1fa742dee48b2293b2e45c1050
Kiel Byrne <kbyrne@costar.com>
2021-04-08 00:31:45 -0400

    remove php fxn

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -109,16 +109,16 @@
     this_form.find('.error-message').slideUp();
     this_form.find('.loading').slideDown();
 
-    if ( $(this).data('recaptcha-site-key') ) {
-      var recaptcha_site_key = $(this).data('recaptcha-site-key');
-      grecaptcha.ready(function() {
-        grecaptcha.execute(recaptcha_site_key, {action: 'php_email_form_submit'}).then(function(token) {
-          php_email_form_submit(this_form,action,this_form.serialize() + '&recaptcha-response=' + token);
-        });
-      });
-    } else {
-      php_email_form_submit(this_form,action,this_form.serialize());
-    }
+    // if ( $(this).data('recaptcha-site-key') ) {
+    //   var recaptcha_site_key = $(this).data('recaptcha-site-key');
+    //   grecaptcha.ready(function() {
+    //     grecaptcha.execute(recaptcha_site_key, {action: 'php_email_form_submit'}).then(function(token) {
+    //       php_email_form_submit(this_form,action,this_form.serialize() + '&recaptcha-response=' + token);
+    //     });
+    //   });
+    // } else {
+    //   php_email_form_submit(this_form,action,this_form.serialize());
+    // }
     
     return true;
   });
