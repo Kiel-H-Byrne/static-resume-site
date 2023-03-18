511b6d5104841a213c96b73c3585c26d2aa56ed9
Kiel Byrne <kiel.byrne@gmail.com>
2023-03-17 23:36:04 -0400

    app with remix

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,70 @@
+import {
+  Box,
+  Container,
+  Divider,
+  Heading,
+  Link,
+  List,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import experience from "../experience.json";
+const formatDate = (date) => {
+  return new Date(date).toLocaleDateString();
+};
 export default function Index() {
+  console.log(experience);
+  const { work, education, awards, skills, basics, certificates } = experience;
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Container>
+      <Box>
+        <Heading as={"h1"} size={"lg"}>
+          CAREER & EDUCATION
+        </Heading>
+        <Divider />
+        <Heading as={"h4"} size={"md"} color={"blackAlpha.500"}>
+          My professional and educational experiences.
+        </Heading>
+        <List
+          sx={{
+            display: "flex",
+            flexFlow: "column wrap",
+            maxHeight: "100%",
+            // width: "150px",
+            overflow: "auto",
+            alignContent: "flex-start",
+          }}
+        >
+          {work.map((w) => (
+            <ListItem
+              key={w.position}
+              sx={{
+                padding: "0 0 20px 20px",
+                marginTop: "-2px",
+                borderLeft: "2px solid #622F75",
+                position: "relative",
+              }}
+            >
+              <Heading as={"h3"} size={"md"}>
+                {w.position}
+              </Heading>
+              <Box dir="column">
+                <Heading as={"h4"} size={"sm"}>
+                  {w.position}
+                </Heading>
+                <Heading as={"h5"} size={"sm"}>
+                  {formatDate(w.startDate)}-{formatDate(w.endDate)}
+                </Heading>
+                <Text as={"p"}>
+                  <Link target={w.url}>{w.name}</Link>
+                </Text>
+                <Text as={"p"}>{w.summary}</Text>
+              </Box>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Container>
   );
 }
